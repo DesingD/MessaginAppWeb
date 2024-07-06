a78dd9e8a1378a312957bfe78358c4abf5a39545
Dany Mitte <mittedany547@gmail.com>
2024-07-06 01:46:07 -0500

    Update: message update scroll

--- frontend/src/components/chat/Messsages.vue
+++ frontend/src/components/chat/Messsages.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <!--Aqui va el codigo de los mensajes-->
-        <div >
+        <div class="overflow-scroll h-[60vh]">
            <!-- <img :src="image" alt="" class="w-10 h-10 rounded-full object-cover">-->
 
-           <ul class=" p-4 flex flex-col gap-6 dark:text-darkTitleChat justify-end">
+           <ul class=" p-4 flex flex-col gap-6 dark:text-darkTitleChat text-[#0E1114] justify-end  ">
                 <li class="flex flex-col items-start " v-for="(message, index) in messages" :key="index" :class="{'items-end': message.send}">
-                    <p class="relative dark:bg-borderidark p-4 rounded-t-lg rounded-br-lg max-w-[90%]" :class="{'text-right rounded-br-none rounded-bl-lg dark:bg-[#0086EA]': message.send}">{{message.message}}</p>
-                    <span class="static left-0" :class="{'text-right right-0': message.send}">10:30</span>
+                    <p class="relative bg-[#EEF1F4] dark:bg-borderidark p-4 rounded-t-lg rounded-br-lg max-w-[90%]" :class="{' rounded-br-none rounded-bl-lg dark:bg-[#0085ea6b] bg-[#d9e8fa]': message.send}">{{message.message}}</p>
+                    <span class="static left-0 text-xs mt-1 text-[#616C76] dark:text-[#ABBBC9]" :class="{'text-right right-0': message.send}">10:30 PM</span>
                 </li>
            </ul>
             <!-- <div class="dark:text-darkTitleChat" v-for="(message, index) in messages" :key="index">
--- frontend/src/helpers/chats/findMessages.js
+++ frontend/src/helpers/chats/findMessages.js
@@ -2,22 +2,27 @@ export function FindMessages () {
     const Messages = [
         {
             send: false,
-            message: "Hello",
+            message: "Handsome lad like you. There must be some special girl",
             date: 1719003546998
         },
         {
             send: false,
-            message: "How Are you?",
+            message: "Come on, what’s her name?",
             date: 1719003577695
         },
         {
             send: true,
-            message: "I'm fine and you?",
+            message: "Well, there is this one girl. I’ve had a crush on her ever since I can remember. But I’m pretty sure she didn’t know I was alive until the reaping",
             date: 1719003597524
         },
         {
             send: false,
-            message: "Fine fine!",
+            message: "She has another fellow?",
+            date: 1719003608611
+        },
+        {
+            send: true,
+            message: "I don’t know, but a lot of boys like her",
             date: 1719003608611
         },
         
